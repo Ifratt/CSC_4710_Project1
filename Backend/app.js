e707fb871142e82d37a997004e72dc5908650d68
Hiba Kabir <162749705+hkabir2k1@users.noreply.github.com>
2024-10-27 16:06:58 -0400

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,39 +1,30 @@
 // Backend: application services, accessible by URIs
 
 
-const express = require('express')
-const cors = require ('cors')
-const dotenv = require('dotenv')
-dotenv.config()
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
 const dbService = require('./dbService');
 
-
 app.use(cors());
-app.use(express.json())
-app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // create
-app.post('/insert', (request, response) => {
-    console.log("app: insert a row.");
-    // console.log(request.body); 
+app.post('/register', (request, response) => {
+    const { username, password, firstname, lastname, salary, age } = request.body;
 
-    const {name} = request.body;
     const db = dbService.getDbServiceInstance();
+    const result = db.registerUser(username, password, firstname, lastname, salary, age);
 
-    const result = db.insertNewName(name);
- 
-    // note that result is a promise
-    result 
-    .then(data => response.json({data: data})) // return the newly added row to frontend, which will show it
-   // .then(data => console.log({data: data})) // debug first before return by response
-   .catch(err => console.log(err));
+    result
+        .then(data => response.json({ success: true, data }))
+        .catch(err => response.status(500).json({ success: false, error: err.message }));
 });
-
-
-
 
 // read 
 app.get('/getAll', (request, response) => {
@@ -48,60 +39,6 @@ app.get('/getAll', (request, response) => {
     .catch(err => console.log(err));
 });
 
-
-app.get('/search/:name', (request, response) => { // we can debug by URL
-    
-    const {name} = request.params;
-    
-    console.log(name);
-
-    const db = dbService.getDbServiceInstance();
-
-    let result;
-    if(name === "all") // in case we want to search all
-       result = db.getAllData()
-    else 
-       result =  db.searchByName(name); // call a DB function
-
-    result
-    .then(data => response.json({data: data}))
-    .catch(err => console.log(err));
-});
-
-
-// update
-app.patch('/update', 
-     (request, response) => {
-          console.log("app: update is called");
-          //console.log(request.body);
-          const{id, name} = request.body;
-          console.log(id);
-          console.log(name);
-          const db = dbService.getDbServiceInstance();
-
-          const result = db.updateNameById(id, name);
-
-          result.then(data => response.json({success: true}))
-          .catch(err => console.log(err)); 
-
-     }
-);
-
-// delete service
-app.delete('/delete/:id', 
-     (request, response) => {     
-        const {id} = request.params;
-        console.log("delete");
-        console.log(id);
-        const db = dbService.getDbServiceInstance();
-
-        const result = db.deleteRowById(id);
-
-        result.then(data => response.json({success: true}))
-        .catch(err => console.log(err));
-     }
-)   
-
 // debug function, will be deleted later
 app.post('/debug', (request, response) => {
     // console.log(request.body); 
@@ -111,21 +48,6 @@ app.post('/debug', (request, response) => {
 
     return response.json({success: true});
 });   
-
-// debug function: use http://localhost:5050/testdb to try a DB function
-// should be deleted finally
-app.get('/testdb', (request, response) => {
-    
-    const db = dbService.getDbServiceInstance();
-
-    
-    const result =  db.deleteById("14"); // call a DB function here, change it to the one you want
-
-    result
-    .then(data => response.json({data: data}))
-    .catch(err => console.log(err));
-});
-
 
 // set up the web server listener
 // if we use .env to configure
