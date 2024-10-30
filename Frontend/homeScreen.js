1dae3622b408a4e28d1403daa0a069be314eed0b
Hiba Kabir <162749705+hkabir2k1@users.noreply.github.com>
2024-10-30 18:42:33 -0400

    Update homeScreen.js

--- Frontend/homeScreen.js
+++ Frontend/homeScreen.js
@@ -1,5 +1,5 @@
 // This is the frontEnd that modifies the HTML page directly
-// event-based programming, such as document load, click a button
+// event-based programming,such as document load, click a button
 
 /*
 What is a Promise in Javascript? 
@@ -25,12 +25,12 @@ Promises have two main methods: then and catch.
 
 What is a promise chain? 
     The Promise chain starts with some asyncOperation1(), which returns a promise, 
-    and each subsequent `then` is used to handle the result of the previous Promise.
+    and each subsequent ``then`` is used to handle the result of the previous Promise.
 
     The catch is used at the end to catch any errors that might occur at any point 
     in the chain.
 
-    Each then returns a new Promise, allowing you to chain additional `then` calls to 
+    Each then returns a new Promise, allowing you to chain additional ``then`` calls to 
     handle subsequent results.
 
 What is an arrow function?
@@ -63,49 +63,105 @@ Arrow functions have a few notable features:
 
 
 // fetch call is to call the backend
+// This is the frontEnd for user registration functionality
+// event-based programming, utilizing DOM events
 
-
-
+// fetch call to check initial state or for any setup
 document.addEventListener('DOMContentLoaded', function() {
-    // one can point your browser to http://localhost:5050/getAll to check what it returns first.
     fetch('http://localhost:5050/getAll')     
     .then(response => response.json())
     .then(data => loadHTMLTable(data['data']));
 });
 
 
-const searchBtn = document.querySelector('#search-btn');
-searchBtn.onclick = function () {
+document.querySelector('#search-btn').onclick = function() {
+    const options = document.querySelector('#search-option').value;
+    
+    switch (options) {
+        case 'searchByName':
+            fetchSearchByName() 
+            break;
+        case 'joinedAfterJohn':
+            fetchUsersAfterJohn() 
+            break;
+        case 'neverSignedIn':
+            fetchUsersNeverSignedIn();
+            break;
+        case 'registeredOnSameDayAsJohn':
+            fetchUsersRegisteredOnSameDayAsJohn();
+            break;
+        case 'registeredToday':
+            fetchUsersRegisteredToday();
+            break;
+       
+    }
+};
+
+function fetchSearchByName(){
     const searchInput = document.querySelector('#search-input');
     const searchValue = searchInput.value.trim();
     searchInput.value = "";
 
-    // Send a request to the backend with the search term
     fetch('http://localhost:5050/search/' + encodeURIComponent(searchValue))
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data.data.length === 0) {
-                console.log("No data found for:", searchValue);
-            } else {
-                console.log("Data found:", data.data);
-            }
-            loadHTMLTable(data['data']);
-        })
+
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data.data.length === 0) {
+            console.log("No data found for:", searchValue);
+        } else {
+            console.log("Data found:", data.data);
+        }
+        loadHTMLTable(data['data']);
+    })
+    .catch(err => {
+        console.error("Fetch error:", err);
+        alert("Error fetching data. Please try again."); // Notify the user of the error
+    });
+}
+
+function fetchUsersAfterJohn() {
+    fetch('http://localhost:5050/search/joinedAfterJohn') // Automatically search after "john"
+        .then(response => response.json())
+        .then(data => loadHTMLTable(data['data']))
         .catch(err => {
-            console.error("Fetch error:", err);
-            alert("Error fetching data. Please try again."); // Notify the user of the error
+            console.error(err);
+            alert("An error occurred.");
         });
-};
+}
 
+function fetchUsersNeverSignedIn() {
+    fetch('http://localhost:5050/search/neverSignedIn')
+        .then(response => response.json())
+        .then(data => loadHTMLTable(data['data']))
+        .catch(err => {
+            console.error(err);
+            alert("An error occurred");
+        });
+}
 
-
-
-
+function fetchUsersRegisteredOnSameDayAsJohn() {
+    fetch('http://localhost:5050/search/registeredOnSameDayAsJohn')
+        .then(response => response.json())
+        .then(data => loadHTMLTable(data['data']))
+        .catch(err => {
+            console.error(err);
+            alert("An error occurred");
+        });
+}
+function fetchUsersRegisteredToday() {
+    fetch('http://localhost:5050/search/registeredToday')
+        .then(response => response.json())
+        .then(data => loadHTMLTable(data['data']))
+        .catch(err => {
+            console.error(err);
+            alert("An error occurred");
+        });
+}
 
 
 // this function is used for debugging only, and should be deleted afterwards
@@ -119,64 +175,69 @@ function debug(data) {
     });
 }
 
-function insertRowIntoTable(data) {
+function insertRowIntoTable(data){
+
     debug("homeScreen.js: insertRowIntoTable called: ");
     debug(data);
-
+ 
     const table = document.querySelector('table tbody');
     debug(table);
-
+ 
     const isTableData = table.querySelector('.no-data');
-
+ 
+   // debug(isTableData);
+ 
     let tableHtml = "<tr>";
-
-    for (var key in data) { // iterating over the each property key of an object data
-        if (data.hasOwnProperty(key)) { // key is a direct property for data
-            if (key === 'registerday') { // the property is 'registerday'
-                data[key] = new Date(data[key]).toLocaleDateString(); // format to javascript date string
-            } else if (key === 'signintime') {
-                data[key] = new Date(data[key]).toLocaleString(); // format to javascript date and time string
-            }
-            tableHtml += `<td>${data[key]}</td>`;
-        }
+    
+    for(var key in data){ // iterating over the each property key of an object data
+       if(data.hasOwnProperty(key)){   // key is a direct property for data
+             if(key === 'dateAdded'){  // the property is 'dataAdded'
+                 data[key] = new Date(data[key]).toLocaleString(); // format to javascript string
+             }
+             tableHtml += `<td>${data[key]}</td>`;
+       }
     }
-
-    tableHtml += `<td><button class="delete-row-btn" data-id=${data.username}>Delete</button></td>`;
-    tableHtml += `<td><button class="edit-row-btn" data-id=${data.username}>Edit</button></td>`;
+ 
+    tableHtml +=`<td><button class="delete-row-btn" data-id=${data.id}>Delete</td>`;
+    tableHtml += `<td><button class="edit-row-btn" data-id=${data.id}>Edit</td>`;
+ 
     tableHtml += "</tr>";
-
-    if (isTableData) {
+ 
+     if(isTableData){
         debug("case 1");
         table.innerHTML = tableHtml;
-    } else {
-        debug("case 2");
-        const newrow = table.insertRow();
-        newrow.innerHTML = tableHtml;
-    }
-}
+     }
+     else {
+         debug("case 2");
+         // debug(tableHtml);
+ 
+         const newrow = table.insertRow();
+         newrow.innerHTML = tableHtml;
+     }
+ }
 
-function loadHTMLTable(data) {
+function loadHTMLTable(data){
     debug("homeScreen.js: loadHTMLTable called.");
 
     const table = document.querySelector('table tbody'); 
-
-    if (data.length === 0) {
+    
+    if(data.length === 0){
         table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
         return;
     }
-
+  
     let tableHtml = "";
-    data.forEach(function ({ username, password, firstname, lastname, salary, age, registerday, signintime }) {
-        tableHtml += "<tr>";
-        tableHtml += `<td>${username}</td>`;
-        tableHtml += `<td>${firstname}</td>`;
-        tableHtml += `<td>${lastname}</td>`;
-        tableHtml += `<td>${salary}</td>`;
-        tableHtml += `<td>${age}</td>`;
-        tableHtml += `<td>${new Date(registerday).toLocaleDateString()}</td>`;
-        tableHtml += `<td>${new Date(signintime).toLocaleString()}</td>`;
-        tableHtml += "</tr>";
+    data.forEach(function ({ username, firstname, lastname, salary, age, registerday}){
+         tableHtml += "<tr>";
+         tableHtml += `<td>${username}</td>`;
+         tableHtml += `<td>${firstname}</td>`;
+         tableHtml += `<td>${lastname}</td>`;
+         tableHtml += `<td>${salary}</td>`;
+         tableHtml += `<td>${age}</td>`;
+         tableHtml += `<td>${new Date(registerday).toLocaleDateString()}</td>`;
+         tableHtml += "</tr>";
     });
 
     table.innerHTML = tableHtml;
 }
+
